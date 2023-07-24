8b44088c933e6989430944a323b1ac3d64d37679
Harshyamsinh Chudasama <140407509+harshyamsinh@users.noreply.github.com>
2023-07-24 19:15:30 +0530

    Add files via upload

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.mygpscpre
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val screen2=findViewById<Button>(R.id.btnGetStart)
+
+        screen2.setOnClickListener{
+            intent= Intent(applicationContext, loginScreen::class.java)
+            startActivity(intent)
+        }
     }
 }
